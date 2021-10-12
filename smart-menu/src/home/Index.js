5bfaa2ce3ce580ccb4680926d069492e0282949c
Altinvs <78910193+Altinvs@users.noreply.github.com>
2021-10-12 19:15:27 +0200

    qr code

--- smart-menu/src/home/Index.js
+++ smart-menu/src/home/Index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { QRCode } from "react-qr-svg";
 import { accountService } from '@/_services';
 
 function Home() {
@@ -10,7 +10,15 @@ function Home() {
             <div className="container">
                 <h1>Hi {user.firstName}!</h1>
                 <p>Welcome to Smart-Menu</p>
+                <QRCode
+                    bgColor="#FFFFFF"
+                    fgColor="#000000"
+                    level="Q"
+                    style={{ width: 128 }}
+                    value="http://localhost:8080/menu"
+                />
             </div>
+            
         </div>
     );
 }
